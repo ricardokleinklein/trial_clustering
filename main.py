7c40affa6e7ecc6310799f9da83be7386e1d1553
Ricardo Kleinlein <ricardokleinklein@gmail.com>
2018-03-16 17:12:12 +0900

    include command line pipeline

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 # encoding: utf-8
+"""
+Clustering of a datafile with no previos knowledge about it
+but the number of attributes.
+
+Usage:
+	main.py <data_dir> <dst_dir>
+
+options:
+	-n, --n_atts			Number of attributes (default:15)
+	-h, --help				Display help
+"""
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
 import os
-import string
-import numpy as np
-
 from docopt import docopt
-from tqdm import tqdm
+from collections import namedtuple
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-from utils import *
+from preprocessing import *
+from features import *
+
+from hparams import hparams
+
+
+def get_hparams(hparams):
+	return namedtuple('hparams',hparams.keys())(*hparams.values())
 
 if __name__ == "__main__":
-	path = '/Users/ricardokleinlein/Desktop/py2/data/data.csv'
-	data = load_data(path)
+	args = docopt(__doc__)
+	data_dir = args["<data_dir>"]
+	dst_dir = args["<dst_dir>"]
+
+	hparams = get_hparams(hparams)
+
+	data = load_data(data_dir)
+	data = normalize(data)
+
 	
 
